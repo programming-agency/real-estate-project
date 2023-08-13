daf53dc7db8ae4a3cbbcbb9d8421efc90f6525c9
HASHEBUR RAHMAN <you@example.com>
2023-08-14 05:47:43 +0600

    :rocket: home

--- src/components/global-components/page-header.js
+++ src/components/global-components/page-header.js
@@ -3,37 +3,37 @@ import { Link } from 'react-router-dom';
 
 class Page_header extends Component {
 
-    render() {
+	render() {
 
-        let HeaderTitle = this.props.headertitle;
-        let publicUrl = process.env.PUBLIC_URL+'/'
-        let Subheader = this.props.subheader ? this.props.subheader : HeaderTitle
+		let HeaderTitle = this.props.headertitle;
+		let publicUrl = process.env.PUBLIC_URL + '/'
+		let Subheader = this.props.subheader ? this.props.subheader : HeaderTitle
 		let CustomClass = this.props.customclass ? this.props.customclass : ''
-        let Img = this.props.Img ? this.props.Img :'14.jpg'
+		let Img = this.props.Img ? this.props.Img : '14.jpg'
 
-        return (
+		return (
 
-		<div className={"ltn__breadcrumb-area text-left bg-overlay-white-30 bg-image "+CustomClass} data-bs-bg={publicUrl+"assets/img/bg/14.jpg"}>
-			<div className="container">
-				<div className="row">
-				<div className="col-lg-12">
-					<div className="ltn__breadcrumb-inner">
-					<h1 className="page-title">{ HeaderTitle }</h1>
-					<div className="ltn__breadcrumb-list">
-						<ul>
-						<li><Link to="/"><span className="ltn__secondary-color"><i className="fas fa-home" /></span> Home</Link></li>
-						<li>{ Subheader }</li>
-						</ul>
+			<div className={"ltn__breadcrumb-area text-left bg-overlay-white-30 bg-image " + CustomClass} data-bs-bg={"/assets/img/bg/14.jpg"}>
+				<div className="container">
+					<div className="row">
+						<div className="col-lg-12">
+							<div className="ltn__breadcrumb-inner">
+								<h1 className="page-title">{HeaderTitle}</h1>
+								<div className="ltn__breadcrumb-list">
+									<ul>
+										<li><Link to="/"><span className="ltn__secondary-color"><i className="fas fa-home" /></span> Home</Link></li>
+										<li>{Subheader}</li>
+									</ul>
+								</div>
+							</div>
+						</div>
 					</div>
-					</div>
-				</div>
 				</div>
 			</div>
-		</div>
 
 
-        )
-    }
+		)
+	}
 }
 
 
--- src/components/section-components/sponsor-v1.js
+++ src/components/section-components/sponsor-v1.js
@@ -4,47 +4,47 @@ import parse from 'html-react-parser';
 
 class Sponsor extends Component {
 
-    render() {
+	render() {
 
-        let publicUrl = process.env.PUBLIC_URL+'/'
+		let publicUrl = process.env.PUBLIC_URL + '/'
 
-    return <div className="ltn__brand-logo-area ltn__brand-logo-1 section-bg-1 pt-110 pb-110 plr--9 d-none---">
-			  <div className="container-fluid">
-			    <div className="row ltn__brand-logo-active">
-			      <div className="col-lg-12">
-			        <div className="ltn__brand-logo-item">
-			          <img src={publicUrl+"assets/img/brand-logo/1.png"} alt="Brand Logo" />
-			        </div>
-			      </div>
-			      <div className="col-lg-12">
-			        <div className="ltn__brand-logo-item">
-			          <img src={publicUrl+"assets/img/brand-logo/2.png"} alt="Brand Logo" />
-			        </div>
-			      </div>
-			      <div className="col-lg-12">
-			        <div className="ltn__brand-logo-item">
-			          <img src={publicUrl+"assets/img/brand-logo/3.png"} alt="Brand Logo" />
-			        </div>
-			      </div>
-			      <div className="col-lg-12">
-			        <div className="ltn__brand-logo-item">
-			          <img src={publicUrl+"assets/img/brand-logo/4.png"} alt="Brand Logo" />
-			        </div>
-			      </div>
-			      <div className="col-lg-12">
-			        <div className="ltn__brand-logo-item">
-			          <img src={publicUrl+"assets/img/brand-logo/5.png"} alt="Brand Logo" />
-			        </div>
-			      </div>
-			      <div className="col-lg-12">
-			        <div className="ltn__brand-logo-item">
-			          <img src={publicUrl+"assets/img/brand-logo/3.png"} alt="Brand Logo" />
-			        </div>
-			      </div>
-			    </div>
-			  </div>
+		return <div className="ltn__brand-logo-area ltn__brand-logo-1 section-bg-1 pt-110 pb-110 plr--9 d-none---">
+			<div className="container-fluid">
+				<div className="row ltn__brand-logo-active">
+					<div className="col-lg-12">
+						<div className="ltn__brand-logo-item">
+							<img src={"/assets/img/brand-logo/1.png"} alt="Brand Logo" />
+						</div>
+					</div>
+					<div className="col-lg-12">
+						<div className="ltn__brand-logo-item">
+							<img src={"/assets/img/brand-logo/2.png"} alt="Brand Logo" />
+						</div>
+					</div>
+					<div className="col-lg-12">
+						<div className="ltn__brand-logo-item">
+							<img src={"/assets/img/brand-logo/3.png"} alt="Brand Logo" />
+						</div>
+					</div>
+					<div className="col-lg-12">
+						<div className="ltn__brand-logo-item">
+							<img src={"/assets/img/brand-logo/4.png"} alt="Brand Logo" />
+						</div>
+					</div>
+					<div className="col-lg-12">
+						<div className="ltn__brand-logo-item">
+							<img src={"/assets/img/brand-logo/5.png"} alt="Brand Logo" />
+						</div>
+					</div>
+					<div className="col-lg-12">
+						<div className="ltn__brand-logo-item">
+							<img src={"/assets/img/brand-logo/3.png"} alt="Brand Logo" />
+						</div>
+					</div>
+				</div>
 			</div>
-        }
+		</div>
+	}
 }
 
 export default Sponsor
