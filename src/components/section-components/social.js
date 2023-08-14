576b046de37b375edccbbd9c59457cd9fa4ed855
HASHEBUR RAHMAN <you@example.com>
2023-08-15 01:22:09 +0600

    :tada:social link

--- src/components/section-components/social.js
+++ src/components/section-components/social.js
@@ -10,12 +10,12 @@ class Social extends Component {
 
 		return <div className="ltn__social-media">
 			<ul>
-				<li><a href="https://www.facebook.com/programming.agency23/" title="Facebook"><i className="fab fa-facebook-f" /></a></li>
-				 
-				<li><a href="https://www.instagram.com/programming_agency/" title="Instagram"><i className="fab fa-instagram" /></a></li>
-				<li><a href="https://www.youtube.com/channel/UCAqMNl8mGM-JVDHowBnYQSA" title="YouTube"><i className="fab fa-youtube"/> </a></li>
+				<li><a target='blank' href="https://www.facebook.com/programming.agency23/" title="Facebook"><i className="fab fa-facebook-f" /></a></li>
+				<li><a target='blank' href="https://web.whatsapp.com/" title="WhatsApp"><i className="fab fa-whatsapp" /></a></li>
+				<li><a target='blank' href="https://www.instagram.com/programming_agency/" title="Instagram"><i className="fab fa-instagram" /></a></li>
+				<li><a target='blank' href="https://www.youtube.com/channel/UCAqMNl8mGM-JVDHowBnYQSA" title="YouTube"><i className="fab fa-youtube" /> </a></li>
 			</ul>
-		
+
 		</div>
 	}
 }
