107c9c6c90a956713929bfd6aaa8ba2c47e6648e
HASHEBUR RAHMAN <you@example.com>
2023-08-15 01:03:13 +0600

    :rocket:social update

--- src/components/section-components/social.js
+++ src/components/section-components/social.js
@@ -11,10 +11,11 @@ class Social extends Component {
 		return <div className="ltn__social-media">
 			<ul>
 				<li><a href="https://www.facebook.com/programming.agency23/" title="Facebook"><i className="fab fa-facebook-f" /></a></li>
-				<li><a href="/" title="Twitter"><i className="fab fa-twitter" /></a></li>
-				<li><a href="/" title="Instagram"><i className="fab fa-instagram" /></a></li>
-				<li><a href="/" title="Dribbble"><i className="fab fa-dribbble" /></a></li>
+				 
+				<li><a href="https://www.instagram.com/programming_agency/" title="Instagram"><i className="fab fa-instagram" /></a></li>
+				<li><a href="https://www.youtube.com/channel/UCAqMNl8mGM-JVDHowBnYQSA" title="YouTube"><i className="fab fa-youtube"/> </a></li>
 			</ul>
+		
 		</div>
 	}
 }
